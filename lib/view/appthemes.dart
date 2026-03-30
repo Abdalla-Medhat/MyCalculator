f2a5224c1aee32aaf0fb674f5a761a0c6e422c4a
Abdallah Medhat <abdallahmedhat.dev@gmail.com>
2026-03-30 23:50:16 +0200

    UI modification: Adding application themes.

---
+++ lib/view/appthemes.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Appthemes {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.light().copyWith(
+      primary: const Color(0xFFD67818),
+      onPrimary: const Color.fromARGB(255, 255, 255, 255),
+      secondary: const Color(0xFFB3BED2),
+      onSecondary: const Color.fromARGB(255, 0, 0, 0),
+      tertiary: const Color(0xFFD67818),
+      onTertiary: const Color.fromARGB(255, 0, 0, 0),
+      error: Colors.red,
+      onError: const Color.fromARGB(255, 255, 255, 255),
+      surface: const Color.fromARGB(255, 255, 255, 255),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        backgroundColor: const Color(0xFFD67818),
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+      ),
+    ),
+  );
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.dark().copyWith(
+      primary: const Color(0xFF2E3443),
+      onPrimary: const Color.fromARGB(255, 255, 255, 255),
+      secondary: const Color(0xFF505B6C),
+      onSecondary: const Color.fromARGB(255, 255, 255, 255),
+      tertiary: const Color(0xFFD67818),
+      onTertiary: const Color.fromARGB(255, 255, 255, 255),
+      error: Colors.red,
+      onError: const Color.fromARGB(255, 255, 255, 255),
+      surface: const Color(0xFF22262F),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: const Color(0xFF2E3443),
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+      ),
+    ),
+  );
+}
