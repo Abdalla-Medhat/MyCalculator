fc1a564750bb689bd8aac5253935eab211dfc935
Abdallah Medhat <abdallahmedhat.dev@gmail.com>
2026-03-30 23:56:34 +0200

    UI modification: Improving status bar appearance.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mycalculator/view/home.dart';
+import 'package:flutter/services.dart'; // => For controlling the status look.
+import 'package:mycalculator/view/appthemes.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Control the status bar color and the icon brightness.
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
   runApp(const MyCalculator());
 }
 
@@ -10,6 +21,13 @@ class MyCalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Appthemes.lightTheme,
+      darkTheme: Appthemes.darkTheme,
+      themeMode: ThemeMode.system,
+      home: Home(),
+      getPages: [GetPage(name: "/", page: () => Home())],
+    );
   }
 }
